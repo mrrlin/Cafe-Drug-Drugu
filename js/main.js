d4bfa29000fca17dc7f825d3dd4fa7b16304f04d
mrrlin <37248550+mrrlin@users.noreply.github.com>
2020-06-18 20:27:21 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// $( document ).ready(function(){
+//     $('#menu .card button.btn').on('click', function () {
+//         $('#menu .collapse').fadeOut('fast');
+//         $(this).parent().parent().parent().find('.collapse').fadeIn('fast');
+//     })
+// })
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
